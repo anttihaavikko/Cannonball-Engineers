e78f0f9bf81329d1d1b16b3694a11a51e7cbea24
Antti Haavikko <antti.haavikko@focus.fi>
2019-11-17 17:00:58 +0200

    Change render pipeline

--- Assets/Scripts/EffectCamera.cs
+++ Assets/Scripts/EffectCamera.cs
@@ -5,14 +5,18 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class EffectCamera : MonoBehaviour {
 
-	private float cutoff = 1f, targetCutoff = 1f;
+    private static readonly float defaultLensDistortion = 20f;
+
+    private float cutoff = 1f, targetCutoff = 1f;
 	private float prevCutoff = 1f;
 	private float cutoffPos = 0f;
 	private float transitionTime = 0.5f;
     public Cinemachine.CinemachineImpulseSource impulseSource;
 
     private PostProcessVolume ppVolume;
-	private float chromaAmount = 0f;
+	private float chromaAmount;
+    private float bulgeAmount = defaultLensDistortion;
+    private float bulgeSpeed = 1f;
 	private float chromaSpeed = 1f;
 
 	private float shakeAmount = 0f, shakeTime = 0f;
@@ -20,6 +24,7 @@ public class EffectCamera : MonoBehaviour {
 	private Vector3 originalPos;
 
     private ChromaticAberration ca;
+    private LensDistortion ld;
 
     public Cinemachine.CinemachineBrain brain;
 
@@ -27,6 +32,7 @@ public class EffectCamera : MonoBehaviour {
         ppVolume = GetComponent<PostProcessVolume>();
         originalPos = transform.position;
         ppVolume.profile.TryGetSettings(out ca);
+        ppVolume.profile.TryGetSettings(out ld);
     }
 
 	void Update() {
@@ -35,6 +41,9 @@ public class EffectCamera : MonoBehaviour {
         {
             chromaAmount = Mathf.MoveTowards(chromaAmount, 0, Time.deltaTime * chromaSpeed);
             ca.intensity.value = chromaAmount;
+
+            bulgeAmount = Mathf.MoveTowards(bulgeAmount, defaultLensDistortion, Time.deltaTime * bulgeSpeed);
+            ld.intensity.value = bulgeAmount;
         }
 
         Time.timeScale = Mathf.MoveTowards(Time.timeScale, 1f, Time.unscaledDeltaTime);
@@ -59,10 +68,17 @@ public class EffectCamera : MonoBehaviour {
 		shakeTime = time;
 	}
 
+    public void Bulge(float amount, float speed)
+    {
+        bulgeAmount = amount;
+        bulgeSpeed = speed;
+    }
+
 	public void BaseEffect(float mod = 1f) {
         impulseSource.GenerateImpulse(Vector3.one * mod * 40f);
         //Shake(0.04f * mod, 0.075f * mod);
         Chromate (1.5f * mod, 2f * mod);
+        Bulge(defaultLensDistortion * mod, 2f * mod);
 
         Time.timeScale = Mathf.Clamp(1f - 0.3f * mod, 0f, 1f);
     }
