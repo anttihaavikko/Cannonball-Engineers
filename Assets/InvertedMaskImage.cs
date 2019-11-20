38ed52f25865303efaff0d7b17ef5800a57f728a
Antti Haavikko <antti.haavikko@focus.fi>
2019-11-20 18:18:57 +0200

    Add start view

---
+++ Assets/InvertedMaskImage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class InvertedMaskImage : Image
+{
+    public override Material materialForRendering
+    {
+        get
+        {
+            Material result = base.materialForRendering;
+            result.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return result;
+        }
+    }
+}
---
+++ Assets/StartView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartView : MonoBehaviour
+{
+    public EffectCamera cam;
+    public Blinders blinders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Glitch();
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            blinders.Close();
+            Invoke("ToLevelSelect", blinders.GetDuration());
+        } 
+    }
+
+    void Glitch()
+    {
+        cam.Chromate(0.3f, 1f);
+        Invoke("Glitch", 1f);
+    }
+
+    void ToLevelSelect()
+    {
+        SceneManager.LoadSceneAsync("Levels");
+    }
+}
